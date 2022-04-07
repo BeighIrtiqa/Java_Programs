a43ee17b55140613b23a3f681205fa5c01ff2b3c
M TAJAMUL <71487217+Tajamul4C@users.noreply.github.com>
2022-04-08 00:40:02 +0530

    Irt.java

--- factorial.java
+++ factorial.java
@@ -1,28 +1,3 @@
-
-//WAP to iteratively calculate factorial of a number
-//Factorial of  5 = 5 x 4 x 3 x 2 x 1 =120
-import java.util.Scanner;
-public class factorial {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int fact = 1, temp;
-		System.out.println("Enter any random integer");
-		int num = sc.nextInt();
-		if(num == 0 || num== 1)
-		{
-			System.out.println("Factorial of "+ num +" =  1");
-		}
-		else
-		{
-			temp = num;
-			while(num >= 1){
-				fact = fact * num;
-				num--;
-			 }
-			System.out.println("Factorial of "+ temp +" = "+fact);
-			}
-		}
-	}
-
-
+//you are really a very hardworking person
+// From 348/500 in 14, and still going on incrementing your grades.
+// Hats off. 💖
